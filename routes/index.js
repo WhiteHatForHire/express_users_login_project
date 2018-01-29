3b45ede16996402acac3b1ed7f308d0f825cfb63
Maxwell Stephens <maxwell@whitehatwebdev.com>
2018-01-29 17:34:05 -0500

    added validation

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,55 @@
 var express = require("express");
 var router = express.Router();
 
+var expressValidator = require('express-validator');
+
+
 /* GET home page. */
 router.get("/register", function(req, res, next) {
   res.render("register", { title: "Registration" });
 });
 
+// add user to db
 router.post("/register", function(req, res, next) {
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
 
-  const db = require("../db.js");
+  // VALIDATION with express-validator
+  req.checkBody('username', 'Username fields cannot be empty.').notEmpty();
+  req.checkBody('username', 'Username must be between 4-15 characters long').len(4, 15);
+  req.checkBody('email', 'The email you entered is invalid, please try again').isEmail();
+  req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
+  req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
+  req.checkBody('password', 'Password must include one lowercase character, one uppercase character, a number, and a special character.').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
+  req.checkBody('passwordMatch', 'Re-entered password must be between 8-100 characters long.').len(8, 100);
+  req.checkBody('passwordMatch', 'Passwords do not match, please try again.').equals(password);
 
-  // Entering with auto escaping to protect against malicious code
-  db.query(
-    "INSERT INTO  users (username, email, password) VALUES (?,?,?)",
-    [username, email, password],
-    function(error, result, fields) {
-      if (error) throw error;
+  const errors = req.validationErrors();
 
-      res.render("register", { title: "Registration Complete" });
-    }
-  );
+  if (errors) {
+    console.log(`errors: ${JSON.stringify(errors)}`);
+
+    res.render("register", { title: "Registration Error", errors: errors });
+    return;
+  } else {
+    const db = require("../db.js");
+
+    // Entering with auto escaping to protect against malicious code
+    db.query(
+      "INSERT INTO  users (username, email, password) VALUES (?,?,?)",
+      [username, email, password],
+      function(error, result, fields) {
+        if (error) throw error;
+
+        res.render("register", {
+          title: "Registration Complete",
+          success: "You have successfully registered"
+        });
+      }
+    );
+  }
+  
 });
 
 module.exports = router;
